f7f5ec0abd65d013a026b5322c366b2884821db8
Dimitry_Mytnikau <kfifa3@gmail.com>
2024-07-30 19:04:07 +0300

    feat: filters

--- app/(root)/_components/artist.tsx
+++ app/(root)/_components/artist.tsx
@@ -1,5 +1,6 @@
 import ArtistCard from '@/app/(root)/_components/artist-card';
 import { TunesType } from '@/app/(root)/_types';
+import Filters from '@/shared/components/filters';
 
 type ArtistProps = {
   search: string;
@@ -14,15 +15,17 @@ const Artist = async ({ search, entity }: ArtistProps) => {
   const data: TunesType = await response.json();
 
   return (
-    <section>
+    <section className="flex flex-col items-center gap-5">
+      {(data.results?.length > 0 || entity) && <Filters />}
+
       {search && data.results?.length === 0 && (
         <p className="text-center text-xl">No results found</p>
       )}
 
-      {data.results?.length > 0 && search && (
+      {data.results?.length > 0 && (
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-          {data.results.map((item) => (
-            <ArtistCard data={item} key={item.trackId} />
+          {data.results.map((item, index) => (
+            <ArtistCard data={item} key={item.trackId + index} />
           ))}
         </div>
       )}
---
+++ shared/components/filters.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import { Tabs, TabsList, TabsTrigger } from '@/shared/components/ui/tabs';
+
+const Filters = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  const handleChange = (value: string) => {
+    if (!value) {
+      params.delete('entity');
+    } else {
+      params.set('entity', value);
+    }
+
+    router.replace(`${pathname}?${params}`, { scroll: false });
+  };
+
+  return (
+    <Tabs defaultValue={params.get('entity') || 'all'}>
+      <TabsList>
+        <TabsTrigger onClick={() => handleChange('')} value="all">
+          All
+        </TabsTrigger>
+        <TabsTrigger onClick={() => handleChange('movie')} value="movie">
+          Movies
+        </TabsTrigger>
+        <TabsTrigger onClick={() => handleChange('audiobook')} value="audiobook">
+          Books
+        </TabsTrigger>
+        <TabsTrigger onClick={() => handleChange('song')} value="song">
+          Songs
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+};
+
+export default Filters;
